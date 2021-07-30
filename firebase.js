e49bd46b4c843ba973cd195cb58e920c920a8c91
Nabzcreates <88214757+Nabzcreates@users.noreply.github.com>
2021-07-30 15:49:07 -0700

    Added new files

---
+++ firebase.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyAlJYbTN3a0oh2HiNVL5jAMsSRd-CQK24w",
+  authDomain: "clone-two-ba33b.firebaseapp.com",
+  projectId: "clone-two-ba33b",
+  storageBucket: "clone-two-ba33b.appspot.com",
+  messagingSenderId: "533894227769",
+  appId: "1:533894227769:web:3d5f73c2df760012816ae8",
+  measurementId: "G-FZ0S18V7LC",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
